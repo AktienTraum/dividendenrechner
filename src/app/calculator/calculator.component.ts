87a627e1d516e90c4df5a3bb296b8eba044c91d3
Thomas Jaspers <thomas.jaspers@codecentric.de>
2024-09-23 17:20:59 +0200

    Read Input Values
    Reading input values using a formgroup and doing some CSS work, so that things look a bit nicer.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, numberAttribute} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatStep, MatStepper} from "@angular/material/stepper";
+import {MAT_DIALOG_DEFAULT_OPTIONS} from "@angular/material/dialog";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSlider, MatSliderThumb} from "@angular/material/slider";
+import {MatFabButton} from "@angular/material/button";
+import {NgIf} from "@angular/common";
+
+export interface Share {
+  purchasePrice: number,
+  currentPrice: number,
+  priceIncrease: number,
+  dividendPercentage: number,
+  dividendPayout: number,
+}
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatStep, MatStepper, ReactiveFormsModule, FormsModule, MatSlider, MatSliderThumb, MatFabButton, NgIf],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {panelClass: 'mat-field-inside-dialog-workaround'}},
+  ]
 })
+
+
 export class CalculatorComponent {
 
+  calculatorForm!: FormGroup;
+
+  currentYear!: number;
+
+  showResult = false;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+    this.initForm();
+  }
+
+  initForm() {
+    this.calculatorForm = new FormGroup({
+      initialInvestment: new FormControl(numberAttribute(10000)),
+      yearlyInvestment: new FormControl(numberAttribute(1000)),
+      yearlyIncrease: new FormControl(numberAttribute(100)),
+      dividendReinvestmentPercentage: new FormControl(numberAttribute(100)),
+      dividendPercentage: new FormControl(numberAttribute(3.5)),
+      dividendPercentageIncrease: new FormControl(numberAttribute(5)),
+
+      years: new FormControl(numberAttribute(10)),
+    });
+  }
+
+  getUntilYear() {
+    return this.currentYear + this.getYears();
+  }
+
+  getYears(): number {
+    return this.calculatorForm.controls['years'].value;
+  }
+
+  doCalculate() {
+    console.log('calculate!');
+    this.showResult = true;
+  }
 }
